67219035909feb255c26047fb5b143f7bb24351e
Cobra034 <sam.heineman034@gmail.com>
2015-01-21 15:31:06 -0500

    object pick up, maze blocked out, camera rotate trigger
    basic pick up and gui for picking up items. blocked out maze. animated
    and created trigger for camera spin

--- Assets/Standard Assets/Character Controllers/ObjectPickup.cs
+++ Assets/Standard Assets/Character Controllers/ObjectPickup.cs
@@ -4,16 +4,30 @@ using System.Collections;
 public class ObjectPickup : MonoBehaviour {
 
 	public int objectCnt = 0;
-	
+	public GameObject camera;
+
+	void Start(){
+		camera = GameObject.FindWithTag("MainCamera");
+	}
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(objectCnt);
+		//Debug.Log(objectCnt);
 	}
 
 	void OnTriggerEnter(Collider other){
 		if(other.gameObject.tag == "Collect"){
 			objectCnt++;
+			Debug.Log(objectCnt);
 			Destroy(other.gameObject);
 		}
+		if(other.gameObject.tag == "CameraRotate"){
+			Debug.Log("Triggered");
+			camera.GetComponent<Animation>().Play();
+			//Destroy(other.gameObject); //If only want to trigger once, use this line of code
+		}
+	}
+
+	void OnGUI(){
+		GUI.Label(new Rect(10, 10, 100, 20), objectCnt+"/5");
 	}
 }
