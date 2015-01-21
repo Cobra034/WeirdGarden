b4df3e6be080e9b52d6819a5056a56fc1091dc64
Cobra034 <sam.heineman034@gmail.com>
2015-01-21 10:25:44 -0500

    Project Setup

---
+++ Assets/Standard Assets/Character Controllers/ObjectPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectPickup : MonoBehaviour {
+
+	public int objectCnt = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(objectCnt);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Collect"){
+			objectCnt++;
+			Destroy(other.gameObject);
+		}
+	}
+}
